9337e8be0a7d0fd189e341c86f2d34a4e684227c
vichivs <vichitravsingh10@outlook.com>
2024-09-13 23:03:39 +0530

    Create automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,4 +1,3 @@
-// Import functions
 import { cart, removeFromCart, updateDeliveryOption } from "../../data/cart.js";
 import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
@@ -30,7 +29,7 @@ export function renderOrderSummary() {
         );
 
         cartSummaryHTML +=`
-        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+        <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
                 Delivery date: ${dateString}
             </div>
@@ -46,15 +45,20 @@ export function renderOrderSummary() {
                 <div class="product-price">
                     $${formatCurrency(matchingProduct.priceCents)}
                 </div>
-                <div class="product-quantity">
+                <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                     <span>
                     Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                     </span>
                     <span class="update-quantity-link link-primary">
                     Update
                     </span>
-                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
-                    Delete
+                    <span class="
+                        delete-quantity-link 
+                        link-primary 
+                        js-delete-link 
+                        js-delete-link-${matchingProduct.id}" 
+                        data-product-id="${matchingProduct.id}">
+                        Delete
                     </span>
                 </div>
                 </div>
